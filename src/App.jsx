82239850a1433f1e235d6b3fc635f9eaa0fbdd6f
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-11-12 12:36:47 +0200

    keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Keyboard from "./components/Keyboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const lowercaseLetters = [];
+
+  for (let i = 97; i <= 122; i++) {
+    lowercaseLetters.push(String.fromCharCode(i));
+  }
+
+  const [text1, setText1] = useState("");
+
+  function handlePressLetter(e) {
+    if (e.target.textContent === "space") {
+      setText1(text1 + " ");
+    } else if (e.target.textContent === "delete") {
+      setText1(text1.slice(0, -1));
+    } else {
+      setText1(text1 + e.target.textContent);
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>{text1}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Keyboard
+        letters={lowercaseLetters}
+        handlePressLetter={handlePressLetter}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
