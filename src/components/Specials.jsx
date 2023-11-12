62a1467502d4f35adfcfc152686ae0cfcf771e45
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-11-12 18:26:38 +0200

    changes and fix

--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -2,27 +2,9 @@ function Special(props) {
   return (
     <div>
       <h3>specials</h3>
-      <button
-        onClick={() => {
-          props.Clear;
-        }}
-      >
-        clearALL
-      </button>
-      <button
-        onClick={() => {
-          props.LowerCase;
-        }}
-      >
-        lowerALL
-      </button>
-      <button
-        onClick={() => {
-          props.UpperCase;
-        }}
-      >
-        upperALL
-      </button>
+      <button onClick={props.Clear}>clearALL</button>
+      <button onClick={props.LowerCase}>lowerALL</button>
+      <button onClick={props.UpperCase}>upperALL</button>
     </div>
   );
 }
