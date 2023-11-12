35cb5b2ed119898e5db4817872e5610336ab237d
chentzriker <chentzriker@hilma.tech>
2023-11-12 13:02:23 +0200

    style component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Keyboard from "./components/Keyboard";
+import StyleText from "./components/StyleText";
 
 function App() {
   const lowercaseLetters = [];
@@ -32,6 +31,7 @@ function App() {
         letters={lowercaseLetters}
         handlePressLetter={handlePressLetter}
       />
+      <StyleText/>
     </>
   );
 }
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Keyboard(props) {
   const keyboard = props.letters.map((letter, index) => (
