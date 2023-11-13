7aaa1bd3f54dcef4959a8a0724a9b4f7d8bd57bd
chentzriker <chentzriker@hilma.tech>
2023-11-13 09:41:22 +0200

    caps button

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -4,17 +4,22 @@ function Keyboard(props) {
       {letter}
     </button>
   ));
-
   const essentials = [",", "/", ".", "?", "!"];
-
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
   const otherBtns = essentials.map((char, index) => (
+    <button id={index} key={index} onClick={(e) => props.handlePressLetter(e)}>
+      {char}
+    </button>
+  ));
+  const numberButtons = numbers.map((char, index) => (
     <button id={index} key={index} onClick={(e) => props.handlePressLetter(e)}>
       {char}
     </button>
   ));
   return (
     <div className="container-keyboard">
-      {keyboard} <br /> {otherBtns} <br />
+      {numberButtons} <br /> {keyboard} <br /> {otherBtns} <br />
+      <button onClick={(e) => props.changeLanguage(e)}>caps</button>
       <button onClick={(e) => props.handlePressLetter(e)}>enter</button>
       <button onClick={(e) => props.handlePressLetter(e)}>space</button>
       <button onClick={(e) => props.handlePressLetter(e)}>delete</button>
--- src/components/StyleText.jsx
+++ src/components/StyleText.jsx
@@ -6,7 +6,8 @@ function StyleText(props) {
       <button onClick = {(e) => props.changeSize(e)}>16</button>
       <button onClick = {(e) => props.changeSize(e)}>18</button>
       <h3>change font style</h3>
-      <button onClick = {(e) => props.changeFontFamily(e)}>Fantasy</button>
+      <button onClick = {(e) => props.changeFontFamily(e)}>Monospace</button>
+      <button onClick = {(e) => props.changeFontFamily(e)}>inter</button>
       <h3>change color</h3>
       <button onClick = {(e) => props.changeColor(e)}>red</button>
       <button onClick = {(e) => props.changeColor(e)}>blue</button>
